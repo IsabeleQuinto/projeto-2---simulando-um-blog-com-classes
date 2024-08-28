dea89120132eec7cff0775c7c3a02ea4ddbca07a
Linda Isabele <151071747+IsabeleQuinto@users.noreply.github.com>
2024-08-28 16:46:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Author = require('./Author')
+
+const john = new Author('John Wick')
+
+const post = john.createPost('As crônicas de John Wick', 'bla bla bla bla')
+
+post.addComments('Sopa', 'Eu amo as crônicas do cara aí')
+post.addComments('Alan', 'Eu odeio as crônicas desse cara muito feio!')
+
+console.log(john)
+console.log(post)
